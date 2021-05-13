942b693328f616d58810c103913e5faf04ab1f41
Yener <yilmazmehmetyener@gmail.com>
2021-05-13 14:18:23 +0300

    initial commit

---
+++ CurrencyModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnomalyDetectionSample
+{
+    public class CurrencyModel
+    {
+        [LoadColumn(0)]
+        public DateTime Date { get; set; }
+
+        [LoadColumn(1)]
+        public float Open { get; set; }
+
+        [LoadColumn(2)]
+        public float High { get; set; }
+
+        [LoadColumn(3)]
+        public float Low { get; set; }
+
+        [LoadColumn(4)]
+        public float Close { get; set; }
+
+        [LoadColumn(5)]
+        public float Volume { get; set; }
+
+        [LoadColumn(6)]
+        public float MarketCap { get; set; }
+    }
+
+    public class SpikeAnomaly
+    {
+        [VectorType(2)]
+        public double[] Anomalies { get; set; }
+    }
+
+    public class PricePrediction
+    {
+        [VectorType(2)]
+        public float[] Predictions { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.TimeSeries;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AnomalyDetectionSample
+{
+    internal class Program
+    {
+        private static string targetFile = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "data.csv");
+
+        private static void Main(string[] args)
+        {
+            var context = new MLContext();
+            var data = context.Data.LoadFromTextFile<CurrencyModel>(targetFile, hasHeader: true, separatorChar: ',');
+
+            Console.WriteLine("***************************\tANOMALY DETECTION\t***************************");
+
+            var pipelineAnomaly = context.Transforms.DetectSpikeBySsa(nameof(SpikeAnomaly.Anomalies),
+                nameof(CurrencyModel.Close), confidence: 98.0,
+                trainingWindowSize: 90, seasonalityWindowSize: 30, pvalueHistoryLength: 20);
+
+            var transformedData = pipelineAnomaly.Fit(data).Transform(data);
+
+            var anomalies = context.Data.CreateEnumerable<SpikeAnomaly>(transformedData, reuseRowObject: false).ToList();
+
+            var prices = data.GetColumn<float>("Close").ToArray();
+            var dates = data.GetColumn<DateTime>("Date").ToArray();
+            for (int i = 0; i < anomalies.Count; i++)
+            {
+                if (anomalies[i].Anomalies[0] == 1)
+                {
+                    Console.WriteLine($"{dates[i]}\t{prices[i]}");
+                }
+            }
+
+            Console.WriteLine("***************************\tPREDICTION BEGINS\t***************************");
+
+            var pipelinePrediction = context.Forecasting.ForecastBySsa(nameof(PricePrediction.Predictions),
+                nameof(CurrencyModel.Close), windowSize: 5,
+                seriesLength: 10, trainSize: 100, horizon: 2);
+            var model = pipelinePrediction.Fit(data);
+            var forecastContext = model.CreateTimeSeriesEngine<CurrencyModel, PricePrediction>(context);
+            var forecasts = forecastContext.Predict(3);
+
+            foreach (var item in forecasts.Predictions)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
